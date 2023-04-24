52d7d46fce4e86d814148d39bf2f94bf2a83403e
lgen0003 <PzBkbza5badaDmzpKbDF>
2023-04-25 04:42:13 +1000

    Created start page

---
+++ WEB-INF/classes/PageStart.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.Connection;
+
+@SuppressWarnings("serial")
+public class PageStart extends HttpServlet {
+    Connection connection;
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        connection = ConnectionUtils.getConnection(config);
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException  {
+        res.setContentType("text/html");
+        PrintWriter toClient = res.getWriter();
+
+        toClient.println(Utils.header("Start"));
+
+        // String city = req.getParameter("ciudad");
+
+        toClient.println("<div class='header header-title'>DKL Bookstore</div>");
+
+        toClient.println("<div class='body'>");
+        toClient.println("<h2>Latest releases</h2>");
+        toClient.println("<div class='cover-row'>");
+
+        Vector<BookData> bookList = BookData.getLatestBooks(connection);
+        for(int i=0; i< bookList.size(); i++){
+                BookData book = bookList.elementAt(i);
+                toClient.println("<img class='cover' src='" + book.cover_image + "'>");
+        }
+
+        toClient.println("</div>");
+
+        toClient.println("<h2>Create an account to browse and buy!</h2");
+        toClient.println("<form action='/Register'><input type='submit' value='Create an account' /></form>");
+        toClient.println("<p>Already have an account? <a href='/Login'>Log in</a></p>");
+
+        toClient.println(Utils.footer());
+        toClient.close();
+    }
+}
---
+++ WEB-INF/classes/Utils.java
@@ -0,0 +1,21 @@
+public class Utils {
+    public static String header(String title) {
+        StringBuilder str = new StringBuilder();
+        str.append("<!DOCTYPE HTML>");
+        str.append("<html>");
+        str.append("<head><title>" + title + "</title>");
+        str.append("<link rel='stylesheet' href='style.css'>");
+        str.append("</head>");
+        str.append("<body>");
+        str.append("<div class='frame'>");
+        return str.toString();
+    }
+
+    public static String footer() {
+        StringBuilder str = new StringBuilder();
+        str.append("</div>");
+        str.append("</body>");
+        str.append("</html>");
+        return str.toString();
+    }
+}
