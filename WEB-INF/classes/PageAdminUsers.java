1b7af45a0d9bab8134740069169373b6b2527f54
Kare Gothner <kare.gothner@gmail.com>
2023-04-25 15:38:18 +0200

    Added admin users list

---
+++ WEB-INF/classes/PageAdminUsers.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.Connection;
+
+@SuppressWarnings("serial")
+public class PageAdminUsers extends HttpServlet {
+    Connection connection;
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        connection = ConnectionUtils.getConnection(config);
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException  {
+        res.setContentType("text/html");
+        PrintWriter toClient = res.getWriter();
+        
+        toClient.println(Utils.header("Users"));
+
+        toClient.println("<div class='header'>");
+        toClient.println("<div class='header-title'>DKL Bookstore</div>");
+        toClient.println("<div><a style='padding-right:15px' href='AdminUsers'>Users</a><a style='padding-right:15px' href='AdminBooks'>Books</a><a href='/''>Sign Out</a></div>");
+        toClient.println("</div>");
+
+        toClient.println("<div class='body'>");
+        toClient.println("<a href='Admin'>< Back</a>");
+        toClient.println("<br>");
+        toClient.println("<br>");
+        toClient.println("<br>");
+
+        toClient.println("<div class='left-right'>");
+        toClient.println("<div><h1>List of Users</h1>");
+        toClient.println("<form><input type='text' placeholder='Search User'><input type='submit' value='Search'></form>");
+        toClient.println("</div>");
+        toClient.println("</div>");
+
+        toClient.println("<br>");
+        toClient.println("<br>");
+
+        toClient.println("<div>");
+        toClient.println("<table class='user-table'>");
+        toClient.println("<div>");
+        toClient.println("<tr>");
+        toClient.println("<td><b>ID</b></td>");
+        toClient.println("<td><b>Email</b></td>");
+        toClient.println("<td><b>First name</b></td>");
+        toClient.println("<td><b>Last name</b></td>");
+        toClient.println("<td><b>City</b></td>");
+        toClient.println("</tr>");
+        toClient.println("</div>");
+
+    /*    
+        int newId = userList.size() + 1;
+        UserData newUser = new UserData(newId, "'abc@hotmail.com'", "'John'", "'Doe'", "'Cali St'", 789, "'West Side'", "'pass88'", false);
+        int nAffected = UserData.registerUser(connection, newUser);
+        userList = UserData.getUserList(connection);
+    */    
+    
+        Vector<UserData> userList = UserData.getUserList(connection);
+     
+        for(int i=0; i< userList.size(); i++){
+                UserData user = userList.elementAt(i);
+                toClient.println("<div>");
+                toClient.println("<tr>");
+                toClient.println("<td>" + user.ID + " </td>");
+                toClient.println("<td>" + user.email + " </td>");
+                toClient.println("<td>" + user.first_name + " </td>");
+                toClient.println("<td>" + user.last_name + " </td>");
+                toClient.println("<td>" + user.city + "</td>");
+                toClient.println("<td><a href='UserEdit?id=" + user.ID + "'>Edit</a></td>");
+                toClient.println("</tr>");
+                toClient.println("</div>");
+        }
+
+        toClient.println("</table>");
+        toClient.println("</div>");
+        toClient.println("<div>");
+        toClient.close();
+    }
+}
